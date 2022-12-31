3c7106bc436eec9436fe2a42b1eb721ecc8bfa4e
bootcoding#123 <ModakAkshali211@gmail.com>
2022-12-31 15:56:42 +0530

    Recursion

---
+++ src/main/java/Recursion/PrintNumber.java
@@ -0,0 +1,15 @@
+package Recursion;
+
+public class PrintNumber {
+    public static void main(String[] args) {
+        int n =5;
+        printNumb(n);
+    }
+    public static void printNumb(int n){
+        if (n == 0){
+            return;
+        }
+        System.out.println(n);
+        printNumb(n-1);
+    }
+}
