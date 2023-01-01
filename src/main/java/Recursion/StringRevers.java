5c7af95d8129b7087155834d824578070ebdfaa4
bootcoding#123 <ModakAkshali211@gmail.com>
2023-01-01 19:14:38 +0530

    Recursion

---
+++ src/main/java/Recursion/StringRevers.java
@@ -0,0 +1,17 @@
+package Recursion;
+
+public class StringRevers {
+    public static void main(String[] args) {
+       String str = "abcd";
+       printRev(str,str.length()-1);
+    }
+    public static void printRev(String str, int indx){
+        if (indx == 0){
+            System.out.print(str.charAt(indx));
+            return;
+        }
+        System.out.print(str.charAt(indx));
+        printRev(str,indx-1);
+
+    }
+}
