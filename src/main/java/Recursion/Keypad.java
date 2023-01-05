eb9b5d35441024b0ecdc502b01985dc7825bc543
bootcoding#123 <ModakAkshali211@gmail.com>
2023-01-05 21:29:36 +0530

    Recursion

---
+++ src/main/java/Recursion/Keypad.java
@@ -0,0 +1,22 @@
+package Recursion;
+
+public class Keypad {
+    public static void main(String[] args) {
+        String str = "23";
+        printComb(str,0,"");
+    }
+    public static String[] keypad = {",","abc","def","ghi","jkl","mno","pqrs","tu","vwx","yz"};
+
+    public static void printComb(String str,int indx,String combination) {
+        if (indx == str.length()){
+            System.out.println(combination);
+            return;
+        }
+        char currChar =  str.charAt(indx);
+        String mapping = keypad[currChar - '0'];
+
+        for (int i=0;i<mapping.length();i++){
+            printComb(str,indx+1,combination+mapping.charAt(i));
+        }
+    }
+}
