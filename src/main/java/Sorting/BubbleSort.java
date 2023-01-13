d2cc4b963601bbe97d09d6ac0196fe293ffdeac4
bootcoding#123 <ModakAkshali211@gmail.com>
2023-01-13 22:48:24 +0530

    BubbleSort

--- src/main/java/Sorting/BubbleSort.java
+++ src/main/java/Sorting/BubbleSort.java
@@ -2,15 +2,15 @@ package Sorting;
 
 public class BubbleSort {
     public static void main(String[] args) {
-        int a[] = {4, 1, 7, 10, 3};
+        int a[] = {4, 5, 7,23, 3};
         for (int i = 0; i < a.length; i++) {
             printBubble(a);
-            System.out.println(a[i]+" ");
+            System.out.print(a[i]+" ");
         }
     }
 
     public static void printBubble(int a[]){
-        for(int i=0; i < a.length; i++){
+        for(int i=0; i < a.length-1; i++){
             for(int j=1; j < (a.length-i); j++){
                 if(a[j-1] > a[j]){
                     //swap elements
