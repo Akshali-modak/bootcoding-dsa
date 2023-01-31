d8b8e8fc98d79eac157045c62c5b540e0b0c96e2
bootcoding#123 <ModakAkshali211@gmail.com>
2023-01-31 22:35:38 +0530

    Check even or odd

--- src/main/java/leetcode/quetions/array/CheckEvenOddInArray.java
+++ src/main/java/leetcode/quetions/array/CheckEvenOddInArray.java
@@ -2,7 +2,7 @@ package leetcode.quetions.array;
 
 public class CheckEvenOddInArray {
     public static void main(String[] args) {
-        int [] a = {6,2,7,3,5};
+        int [] a = {6,2,7,17,5,22,30,46};
         System.out.println("odd no");
         for (int i=0;i<a.length;i++) {
             if (a[i] % 2 != 0) {
