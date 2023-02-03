3c0fdca76503a405b9fe45bbad588bc6dbdbe6a4
bootcoding#123 <ModakAkshali211@gmail.com>
2023-02-03 23:31:24 +0530

    CodeRefactory

--- src/main/java/Recursion/CheckSortedArr.java
+++ src/main/java/Recursion/CheckSortedArr.java
@@ -2,11 +2,11 @@ package Recursion;
 
 public class CheckSortedArr {
     public static void main(String[] args) {
-   int arr[] = {1,3,5};
+   int arr[] = {1,3,5,6};
         System.out.println(isSorted(arr,0));
     }
     public static boolean isSorted(int arr[], int indx){
-        if (indx ==arr.length-1){
+        if (indx == arr.length-1){
             return true;
         }
         if (arr[indx] < arr[indx+1]){
