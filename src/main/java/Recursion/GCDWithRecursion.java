d3adfa50154edcb8fb27fdca80b48c58c7bab549
bootcoding#123 <ModakAkshali211@gmail.com>
2023-02-17 21:03:22 +0530

    GCD

--- src/main/java/Recursion/GCDWithRecursion.java
+++ src/main/java/Recursion/GCDWithRecursion.java
@@ -1,15 +1,12 @@
 package Recursion;
 
 import java.util.Arrays;
-
-
-
 public class GCDWithRecursion {
     public static void main(String[] args) {
-        int[] nums = new int[0];
+        int[] nums = new int[7];
 //        int minNum = nums[0], maxNum = nums[nums.length - 1];
 //        System.out.println();
-        getGCD(4,9);
+        System.out.println( getGCD(8,15));
     }
     public static int getGCD(int a, int b) {
         if (a > b) {
