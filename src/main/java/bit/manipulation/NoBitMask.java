11eda665d514fcacd90358121a56fa57d4ca4b04
bootcoding#123 <ModakAkshali211@gmail.com>
2023-02-18 23:28:19 +0530

    Bit Manipulation programs

---
+++ src/main/java/bit/manipulation/NoBitMask.java
@@ -0,0 +1,13 @@
+package bit.manipulation;
+
+public class NoBitMask {
+    public static void main(String[] args) {
+        int n = 5;//0101
+        int pos = 2;
+        int bitMask = 1<<pos;
+        int notBitMask = ~(bitMask);
+
+        int newNumber = notBitMask & n;
+        System.out.println(newNumber);
+    }
+}
