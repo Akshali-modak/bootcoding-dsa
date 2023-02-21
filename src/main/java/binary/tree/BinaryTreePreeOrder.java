d1a8b8eb26e24ed527ec43a167869349c51387b0
bootcoding#123 <ModakAkshali211@gmail.com>
2023-02-21 22:05:01 +0530

    Binary tree practice PreOrder ex

--- src/main/java/binary/tree/BinaryTreePreeOrder.java
+++ src/main/java/binary/tree/BinaryTreePreeOrder.java
@@ -1,11 +1,15 @@
 package binary.tree;
 
+import static binary.tree.BinaryTreePreeOrder.BinaryTree.preOrderTree;
+
 public class BinaryTreePreeOrder {
     public static void main(String[] args) {
-        int nodes[] = {1,2,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        int nodes[] = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
-        System.out.println(root.data);
+        preOrderTree(root);
+//        System.out.println(root.data);
+
     }
     static class Node{
         int data;
@@ -29,6 +33,19 @@ public class BinaryTreePreeOrder {
             newNode.left = buildTree(nodes);
             newNode.right = buildTree(nodes);
             return newNode;
+        }
+
+
+
+
+
+        public static void preOrderTree(Node root){
+            if (root == null){
+                return;
+            }
+            System.out.println(root.data+" ");
+            preOrderTree(root.left);
+            preOrderTree(root.right);
         }
     }
 }
