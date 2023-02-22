5f87c19ee78903c37665ab0a5499fa46b9e42e6c
bootcoding#123 <ModakAkshali211@gmail.com>
2023-02-22 21:51:20 +0530

    Find Greatest common divisor...

---
+++ src/main/java/leetcode/quetions/FindGreatestCD.java
@@ -0,0 +1,23 @@
+package leetcode.quetions;
+
+import java.util.Arrays;
+
+public class FindGreatestCD {
+    public static void main(String[] args) {
+       int  nums[] = {2,5,6,9,10};
+        System.out.println(findGCD(nums));
+    }
+    public static int findGCD(int[] nums) {
+        Arrays.sort(nums);
+        int max =nums[nums.length-1];
+        int min = nums[0];
+        int sum = 0;
+        for (int i=1; i<=max; i++) {
+            if (min % i == 0 && max % i == 0){
+                sum = i;
+            }
+        }
+        return sum;
+    }
+
+}
