51175399c1685c7b6a2bb262e234ab59b859d825
bootcoding#123 <ModakAkshali211@gmail.com>
2023-02-23 21:00:04 +0530

    Equals two array return......

---
+++ src/main/java/leetcode/quetions/array/EqualsTwoArray.java
@@ -0,0 +1,24 @@
+package leetcode.quetions.array;
+
+public class EqualsTwoArray {
+    public static void main(String[] args) {
+     int[] array1 = {3,4,7,8,2};
+     int[] array2 = {3,4,7,8,2};
+        boolean twoArray = returnEqualsArray(array1, array2);
+        System.out.println(twoArray);
+    }
+
+    public static boolean returnEqualsArray(int[] array1, int[] array2){
+        int lenght1 = array1.length;
+        int lenght2 = array2.length;
+        if (lenght1 != lenght2){
+            return false;
+        }
+        for (int i=0;i<array1.length;i++){
+            if (array1[i] != array2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
