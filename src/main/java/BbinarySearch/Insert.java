a16bb1c501b64cc4f53cbb55ac3ba4afc7e98804
bootcoding#123 <ModakAkshali211@gmail.com>
2023-02-25 20:43:29 +0530

    Binary Search Tree Inser   Program.....

---
+++ src/main/java/BbinarySearch/Insert.java
@@ -0,0 +1,44 @@
+package BbinarySearch;
+
+public class Insert {
+    public static void main(String[] args) {
+     int[] value = {5,1,3,2,4,7};
+     Node root = null;
+     for (int i=0;i<value.length;i++){
+         root=insert(root,value[i]);
+     }
+     inOrder(root);
+        System.out.println( );
+    }
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+    public static Node insert(Node root, int value){
+        if (root == null){
+            root = new Node(value);
+            return root;
+        }
+        if (root.data > value){
+            //left subtree
+            root.left = insert(root.left, value);
+        }
+        else {
+            root.right = insert(root.right, value);
+        }
+        return root;
+    }
+    public static void inOrder(Node root){
+        if (root == null){
+            return;
+        }
+        inOrder(root.left);
+        System.out.print(root.data+" ");
+        inOrder(root.right);
+    }
+}
